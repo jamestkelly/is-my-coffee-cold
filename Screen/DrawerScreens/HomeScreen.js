105b5719ac36b737f1b68a550fa411773e74aec9
Jim <jimkelly.t@outlook.com>
2020-12-10 23:37:34 +1000

    Added todo's and future direction
    Need to do further research on the implementation of some elements before implementing the code to calculate the coffee temperature changing with user weather data selection.

--- Screen/DrawerScreens/HomeScreen.js
+++ Screen/DrawerScreens/HomeScreen.js
@@ -1,8 +1,21 @@
 // Import React and Component
-import React from 'react';
-import {View, Text, SafeAreaView} from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, SafeAreaView } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
+
+// TODO:
+//      ~ Implement code to parse the country name & code list for use in a 'picker'
+//        wheel
+//      ~ Implement code to parse the city name for use in a 'picker' wheel
+//      ~ Implement code to select coffee type preference from a 'picker' wheel
+//      ~ Have the application run the calculation using modified Euler to calculate how long
+//        the user has until the coffee changes flavour
+//      ~ Display time until the coffee changes flavour as a count-down timer on screen
 
 const HomeScreen = () => {
+  const [userCountry, setCountry] = useState('Australia');
+  const [userCity, setCity] = useState('Brisbane');
+  const [userCoffee, setCoffee] = useState(2);
   return (
     <SafeAreaView style={{flex: 1}}>
       <View style={{flex: 1, padding: 16}}>
@@ -18,10 +31,11 @@ const HomeScreen = () => {
               textAlign: 'center',
               marginBottom: 16,
             }}>
-            Home Screen Placeholder
-            {'\n\n'}
-            TODO: Add operations to run coffee calculation
+            Please select the country.
           </Text>
+          <Picker>
+
+          </Picker>
         </View>
       </View>
     </SafeAreaView>
