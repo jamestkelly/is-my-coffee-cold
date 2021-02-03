a18d83cc3cf6fae57865116dc38e5220ad33af4c
jamestkelly <jimkelly.t@outlook.com>
2021-02-04 00:40:09 +1000

    Small Typo Fix

--- src/app/preferences/make-preference/make-preference.page.ts
+++ src/app/preferences/make-preference/make-preference.page.ts
@@ -1,25 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
-import { FormGroup, FormBuilder } from "@angular/forms";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 import { Router } from '@angular/router';
 import { CoffeePreferenceService } from 'src/app/services/coffee-preference.service';
+
+interface PreferenceData {
+  preferenceName: string;
+  coffeeType: string;
+  cityName: string;
+  countryName: string;
+}
 
 @Component({
   selector: 'app-make-preference',
   templateUrl: './make-preference.page.html',
   styleUrls: ['./make-preference.page.scss'],
 })
-export class MakePreferencePage implements OnInit {
-  preferenceForm: FormGroup;
+export class MakePreferencePage {
   preferenceList = [];
+  preferenceData: PreferenceData;
+  preferenceForm: FormGroup;
 
   constructor
   (
-    private coffeePref: CoffeePreferenceService,
-    private route: Router,
+    private coffeeService: CoffeePreferenceService,
     public location: Location,
     public fb: FormBuilder
-  ) { }
+  )
+  {
+    this.preferenceData = {} as PreferenceData;
+  }
 
   ngOnInit() {
     this.preferenceForm = this.fb.group({
@@ -28,25 +38,34 @@ export class MakePreferencePage implements OnInit {
       cityName: [''],
       coffeeType: ['']
     })
+    this.coffeeService.readPreferences().subscribe(data => {
+      this.preferenceList = data.map(output => {
+        return {
+          id: output.payload.doc.id,
+          isEdit: false,
+          preferenceName: output.payload.doc.data()['preferenceName'],
+          coffeeType: output.payload.doc.data()['coffeeType'],
+          cityName: output.payload.doc.data()['cityName'],
+          countryName: output.payload.doc.data()['countryName']
+        };
+      })
+      console.log(this.preferenceList);
+    });
+  }
+
+  createPreference() {
+    console.log(this.preferenceForm.value);
+    this.coffeeService.createPreference(this.preferenceForm.value).then(response => {
+      this.preferenceForm.reset();
+      console.log(response);
+      this.location.back();
+    })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
   goBack() {
     this.location.back();
-  }
-
-  formSubmit() {
-    if (!this.preferenceForm.valid)
-    {
-      return false;
-    }
-    else
-    {
-      this.coffeePref.createPreference(this.preferenceForm.value).then(result =>{
-        console.log(result);
-        this.preferenceForm.reset();
-        this.route.navigate(['/preferences']);
-      })
-        .catch(error => console.log(error));
-    }
   }
 }
