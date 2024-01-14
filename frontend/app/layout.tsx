8aa4754b0abeb90218ff1130b0d8ec8252ccd69c
jamestkelly <jimkelly.t@outlook.com>
2024-01-14 13:22:52 +1000

    feat: implemented base header functionality

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Footer from './components/footer/footer'
+import Header from './components/header/header'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -16,7 +18,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        {children}
+        <Footer />
+      </body>
     </html>
   )
 }
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,20 +1,43 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        "black-primary": "#1E1E1E",
+        "black-secondary": "#21272A",
+        "white-primary": "#FFFAFA",
+        "grey-primary": "#A6A6A6",
+        "primary-0": "#113946",
+        "primary-1": "#2F5562",
+        "primary-2": "#163C5F",
+        "secondary-0": "#EAD7BB",
+        "secondary-1": "#D5CEA3",
+        "secondary-2": "#BCA37F",
+        "tertiary-0": "#413F35",
+        "tertiary-1": "#3F301B",
+        "info-border": "#0D46AB",
+        "info-bg": "#B3D4FF",
+        "warn-border": "#FF791B",
+        "warn-bg": "#FFF1B4",
+        "error-border": "#C2271D",
+        "error-bg": "#FFBDAD",
+        "success-border": "#206C36",
+        "success-bg": "#ABF5D2",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+
+export default config;
